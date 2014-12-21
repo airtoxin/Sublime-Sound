de2fe4ffea4e500c4b8fdfc4ec41f01ed5ebce48
airtoxin <airtoxin@icloud.com>
2014-12-22 06:43:54 +0900

    Remove reflections

--- Sound.py
+++ Sound.py
@@ -22,64 +22,63 @@ class EventSound(sublime_plugin.EventListener):
     def __init__(self, *args, **kwargs):
         super(EventSound, self).__init__(*args, **kwargs)
         self.play = getattr(self, sublime.platform() + '_play')
-        # sublime.packages_path doesn't work here...
-        self.filepaths = {}
 
-    # async play
     @thread
-    def _play(self, dirname, func):
+    def osx_play(self, event_name):
         self.on_play_flag = False
-        dir_path = join(sublime.packages_path(), "Sound", "sounds", dirname)
-        # reduce file access
-        sound_files = self.filepaths.setdefault(dir_path, self._get_sound_files(dir_path))
-        if not len(sound_files) == 0:
-            func(dir_path, sound_files)
-
-    def osx_play(self, dirname):
-        def func(dir_path, sound_files):
-            volume = self.get_volume()
-            call(["afplay", "-v", str(volume / 100), join(dir_path, choice(sound_files))])
-        self._play(dirname, func)
-
-    def win_play(self, dirname):
-        def func(dir_path, sound_files):
-            winsound.PlaySound(join(dir_path, choice(sound_files)), winsound.SND_FILENAME | winsound.SND_ASYNC)
-        self._play(dirname, func)
-
-    def linux_play(self, dirname):
-        def func(dir_path, sound_files):
-            call(["aplay", join(dir_path, choice(sound_files))])
-        self._play(dirname, func)
-
-    def _get_sound_files(self, dir_path):
+        dir_path = join(sublime.packages_path(), "Sound", "sounds", event_name)
         if exists(dir_path):
-            return [f for f in listdir(dir_path) if f.endswith(".wav") ]
-        else:
-            return []
+            sound_files = [f for f in listdir(dir_path) if f.endswith(".wav") ]
+            if not len(sound_files) == 0:
+                volume = self.get_volume()
+                call(["afplay", "-v", str(volume / 100), join(dir_path, choice(sound_files))])
 
-    def on_new(self, view):
+    @thread
+    def win_play(self, event_name):
+        self.on_play_flag = False
+        dir_path = join(sublime.packages_path(), "Sound", "sounds", event_name)
+        if exists(dir_path):
+            sound_files = [f for f in listdir(dir_path) if f.endswith(".wav") ]
+            if not len(sound_files) == 0:
+                winsound.PlaySound(join(dir_path, choice(sound_files)), winsound.SND_FILENAME | winsound.SND_ASYNC)
+
+    @thread
+    def linux_play(self, event_name):
+        self.on_play_flag = False
+        dir_path = join(sublime.packages_path(), "Sound", "sounds", event_name)
+        if exists(dir_path):
+            sound_files = [f for f in listdir(dir_path) if f.endswith(".wav") ]
+            if not len(sound_files) == 0:
+                call(["aplay", join(dir_path, choice(sound_files))])
+
+    def on_new_async(self, view):
+        # Called when a new buffer is created. Runs in a separate thread, and does not block the application.
         self.throttle(lambda: self.play("on_new"), 100)
 
-    def on_clone(self, view):
+    def on_clone_async(self, view):
+        # Called when a view is cloned from an existing one. Runs in a separate thread, and does not block the application.
         self.throttle(lambda: self.play("on_clone"), 100)
 
-    def on_load(self, view):
+    def on_load_async(self, view):
+        # Called when the file is finished loading. Runs in a separate thread, and does not block the application.
         self.throttle(lambda: self.play("on_load"), 100)
 
     def on_close(self, view):
+        # Called when a view is closed (note, there may still be other views into the same buffer).
         self.throttle(lambda: self.play("on_close"), 100)
 
-    def on_pre_save(self, view):
+    def on_pre_save_async(self, view):
+        # Called after a view has been saved. Runs in a separate thread, and does not block the application.
         self.throttle(lambda: self.play("on_save"), 100)
 
-    def on_modified(self, view):
+    def on_modified_async(self, view):
+        # Called after changes have been made to a view. Runs in a separate thread, and does not block the application.
         self.throttle(lambda: self.play("on_modify"), 100)
 
     def throttle(self, func, time):
         # Creates a function that, when executed, will only call the func function at most once per every time milliseconds.
         if not hasattr(self, "on_play_flag"): self.on_play_flag = False
         if self.on_play_flag: return
-        # this flag falsed when sounding complete
         self.on_play_flag = True
         sublime.set_timeout(func, time)
 
